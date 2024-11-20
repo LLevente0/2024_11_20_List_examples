a7618279c4a8edfc2892615d28e87655f6fe0bb6
LLevente <ladanyi.levente@diak.szbi-pg.hu>
2024-11-20 13:19:07 +0100

    a

--- 00_lista_bejarasa.py
+++ 00_lista_bejarasa.py
@@ -3,10 +3,14 @@ gyumolcsok = ['alma', 'körte', 'szilva', 'barack']
 
 print(gyumolcsok)
 
+for gyumolcs in  gyumolcsok:
+    print(gyumolcs)
+
 # hónapok kiíratása
 honapok = ['január', 'február','március', 'április', 'május', 'június', 'július'] 
 
-print(honapok)
+for honap in honapok:
+    print(honap)
 
 # index megjelenítése:
     # 0 január
