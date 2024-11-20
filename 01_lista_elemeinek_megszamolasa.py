03af7cbbaf8ff76bbaad208eee6f230b4f4fafff
LLevente <ladanyi.levente@diak.szbi-pg.hu>
2024-11-20 13:31:16 +0100

    Update

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,12 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+
+szamok = [5, 8, 12, 15, 69]
+print(len(szamok))
+
+darab = 0
+for szam in szamok:
+    darab += 1
+
+print(darab)
